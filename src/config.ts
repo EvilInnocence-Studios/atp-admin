afa5f296451d716afea87d32bded29e068c10dc6
Andrew Wittrock <andy@wittrock.us>
2024-12-13 22:00:43 -0600

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+
+export const config = {
+    appName: "Admin",
+    api: {
+        baseUrl: "http://192.168.254.54:3002/"
+    },
+    gallery: {
+        maxRowCount: 9999,
+    },
+    modules: [
+        "uac",
+    ],
+}
