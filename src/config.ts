183efeffa30e42105ddc281127641c538b238a1d
Andrew Wittrock <andy@wittrock.us>
2025-06-13 21:23:48 -0500

    Add core settings

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { adminMenus } from "@admin/menux";
 import { adminRoutes } from "@admin/routes";
 import { commonRoutes } from "@common/lib/routes";
 import { commonSettings } from "@common/lib/settings";
-import { IApiConfig } from "@core/lib/types";
+import { IConfig } from "@core/lib/types";
 import { storeSettings } from "@store/lib/settings";
 import { uacMenus } from "@uac/lib/menus";
 import { uacRoutes } from "@uac/lib/routes";
@@ -10,8 +10,10 @@ import { commonMenus } from "./common/lib/menus";
 import { localConfig } from "./config.local";
 import { storeMenus } from "./store/lib/menus";
 import { storeRoutes } from "./store/lib/routes";
+import { coreSettings } from "@core/lib/settings";
+import { merge } from "lodash";
 
-export const config = ():IApiConfig => ({
+export const config = ():IConfig => ({
     api: {
         baseUrl: localConfig.api.baseUrl,
     },
@@ -30,10 +32,11 @@ export const config = ():IApiConfig => ({
         ...commonRoutes.admin,
         ...storeRoutes.admin,
     ],
-    settings: {
-        ...commonSettings,
-        ...storeSettings,
-    },
+    settings: merge(
+        commonSettings,
+        storeSettings,
+        coreSettings,
+    ),
     paypal: {
         plans: [],
     }
