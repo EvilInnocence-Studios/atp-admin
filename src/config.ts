0fea88120773ddce723823064c2d751a59852fc8
Andrew Wittrock <andy@wittrock.us>
2025-06-26 21:13:32 -0500

    Combine module exports into one object

--- src/config.ts
+++ src/config.ts
@@ -1,43 +1,20 @@
-import { adminMenus } from "@admin/menux";
-import { adminRoutes } from "@admin/routes";
-import { commonRoutes } from "@common/lib/routes";
-import { commonSettings } from "@common/lib/settings";
-import { IConfig } from "@core/lib/types";
-import { storeSettings } from "@store/lib/settings";
-import { uacMenus } from "@uac/lib/menus";
-import { uacRoutes } from "@uac/lib/routes";
-import { commonMenus } from "./common/lib/menus";
+import { makeConfig } from "@core/lib/makeConfig";
 import { localConfig } from "./config.local";
-import { storeMenus } from "./store/lib/menus";
-import { storeRoutes } from "./store/lib/routes";
-import { coreSettings } from "@core/lib/settings";
-import { merge } from "lodash";
 
-export const config = ():IConfig => ({
-    api: {
-        baseUrl: localConfig.api.baseUrl,
-    },
-    modules: [
-        "uac", "common", "admin"
-    ],
-    menus: [
-        ...adminMenus.admin,
-        ...commonMenus.admin,
-        ...uacMenus.admin,
-        ...storeMenus.admin,
-    ],
-    routes: [
-        ...adminRoutes.admin,
-        ...uacRoutes.admin,
-        ...commonRoutes.admin,
-        ...storeRoutes.admin,
-    ],
-    settings: merge(
-        commonSettings,
-        storeSettings,
-        coreSettings,
-    ),
-    paypal: {
-        plans: [],
-    }
-});
+// Import the modules you want active here
+import { module as adminModule } from "@admin/index";
+import { module as commonModule } from "@common/index";
+import { module as storeModule } from "@store/index";
+import { module as subscriptionsModule } from "@subscription/index";
+import { module as uacModule } from "@uac/index";
+
+// List the modules you want active here
+const modules = [
+    adminModule,
+    commonModule,
+    uacModule,
+    subscriptionsModule,
+    storeModule,
+];
+
+export const config = makeConfig(localConfig.api.baseUrl, modules);
